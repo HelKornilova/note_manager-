a4865819877f4ebdbe353bc3b0f35c8f25c237eb
HelKornilova <elenatutkina@gmail.com>
2024-12-19 22:25:24 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Елена"
+print("Имя пользователя: ", username)
+title = "Grade 1"
+print("Заголовок заметки: ", title)
+content = "Этап 1"
+print("Описание заметки: ", content)
+status = "В работе"
+print("Статус заметки: ", status)
+created_date = "18-12-2024"
+print("Дата создания заметки: ", created_date)
+issue_date = "18-01-2025"
+print("Дата истечения заметки: ", issue_date)
